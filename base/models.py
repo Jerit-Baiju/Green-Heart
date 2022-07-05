1665233c8d1d5444c071d837be4722d6d036b24f
Jerit Baiju <jeritalumkal@gmail.com>
2022-07-05 12:29:30 +0530

    .

--- base/models.py
+++ base/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class ProductPack(models.Model):
+    quantity = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    packs = models.ForeignKey(ProductPack, on_delete=models.CASCADE)
+
