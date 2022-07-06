cc0ee76d34400232a53b7356366fa6faea0f4666
Jerit Baiju <jeritalumkal@gmail.com>
2022-07-06 11:58:50 +0530

    .

--- adminpanel/urls.py
+++ adminpanel/urls.py
@@ -4,7 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.home, name='admin-home'),
     path('add_product/', views.add_product, name='add-product'),
-    path('add_pack/', views.add_pack, name='add-pack'),
-    path('add_category/', views.add_category, name='add-category'),
-    path('categories/', views.categories, name='categories'),
 ]
