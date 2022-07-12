886d76e4d8f48f8bc59940bfeb83a0aecc0d9fa5
Jerit Baiju <jeritalumkal@gmail.com>
2022-07-12 10:44:54 +0530

    .

--- static/js/product.js
+++ static/js/product.js
@@ -3,9 +3,13 @@ document.getElementById('total').innerText = "Total: Rs." + main_pack + ".00/-"
 function refresh_pack() {
   pack = document.getElementById("pack").value;
   quantity = document.getElementById('quantity').value
-  alert(quantity)
   rate = price[pack - 1]
-  document.getElementById("rate").innerText = "Rate: Rs." +' rate' + ".00";
+  document.getElementById("rate").innerText = "Rate: Rs." +rate + ".00";
   total = rate * quantity
   document.getElementById('total').innerText = "Total: Rs." + total + ".00/-"
+}
+
+function increase() {
+  document.getElementById("quantity").innerText = '34'
+    // document.getElementById("quantity").value + 1;
 }
