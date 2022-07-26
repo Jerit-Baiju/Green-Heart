42e71ce63d70d3f8c0b101cb88a73c31389606ca
Jerit Baiju <jeritalumkal@gmail.com>
2022-07-26 11:11:32 +0530

    removed custom admin panel

--- adminpanel/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='admin-home'),
-    path('add_product/', views.add_product, name='add-product'),
-]
